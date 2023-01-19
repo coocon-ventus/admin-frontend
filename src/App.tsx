b54629dc1ef208828e860443073779dd5da73ab1
LAPTOP-SL0JNDB7\NAMJAEMIN <nionu0430@gmail.com>
2023-01-19 17:00:31 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// routing
+import Routes from './routes';
+
+// defaultTheme
+import themes from './themes';
+
+// project imports
+import NavigationScroll from './layout/NavigationScroll';
+import {BrowserRouter} from "react-router-dom";
+
+function App() {
+    // @ts-ignore
+    const customization = useSelector((state) => state.customization);
+  return (
+      <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={themes(customization)}>
+              <CssBaseline />
+              <BrowserRouter>
+                  <NavigationScroll>
+                        <Routes/>
+                  </NavigationScroll>
+              </BrowserRouter>
+          </ThemeProvider>
+      </StyledEngineProvider>
+  );
+}
+
+
+export default App;
